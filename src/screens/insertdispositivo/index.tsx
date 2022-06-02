57c4226c48cf61b997fa7a4e8b8c5e7bb3e4ec3d
tarcisiovasconcelos <tarcisioclovis@gmail.com>
2022-06-01 23:18:31 -0300

    att

--- src/screens/insertdispositivo/index.tsx
+++ src/screens/insertdispositivo/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { BoasVindas } from '../../componentes/home/boasvindas';
 import { FlatSlot } from '../../componentes/slots/flatlistslot';
 
 
