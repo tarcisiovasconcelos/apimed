023b885b11aa666ac97de4750d8c6f6fabbf195e
tarcisiovasconcelos <tarcisioclovis@gmail.com>
2022-06-10 19:06:15 -0300

    COMIT PORRA CONSEGUI

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 import { firebaseConfig } from './src/config/firebase';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Setting a timer']);
 
 firebase.initializeApp(firebaseConfig);
 
