91bafc700662b27650f09ee4d4bdc1508a8545ee
tarcisiovasconcelos <tarcisioclovis@gmail.com>
2022-07-04 11:05:50 -0300

    logica

--- src/componentes/home/infodisp.tsx
+++ src/componentes/home/infodisp.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { getAuth } from 'firebase/auth';
+import { useState } from 'react';
+
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { child, getDatabase, onValue, ref } from 'firebase/database';
 
 export interface InfoDispProps {
 }
 
 export function InfoDisp (props: InfoDispProps) {
+    const auth = getAuth()
+    const usuarioID = auth.currentUser.uid;
+    const database = getDatabase();
+    let [nome] = useState();
+    
+    const refNome = child(child(ref(database, 'dispositivos'), usuarioID), 'nome');
+
+    onValue(refNome, (snapshot) => {
+      console.log(snapshot.val())
+      nome = (snapshot.val());
+      })
 
 
     return (
@@ -13,7 +30,7 @@ export function InfoDisp (props: InfoDispProps) {
             Nome do dispositivo
           </Text>
           <Text style={styles.text1}>
-            Robô do vô beto
+            {nome}
           </Text>
           <Text style={styles.title2}>
             Nome do paciente 
