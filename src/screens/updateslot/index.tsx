b9bffc43f29ccf5123ef4a438f22a6b00fb84679
tarcisiovasconcelos <tarcisioclovis@gmail.com>
2022-09-23 16:03:05 -0300

    fui

--- src/screens/updateslot/index.tsx
+++ src/screens/updateslot/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Alert, StyleSheet, View } from 'react-native';
 import { Button, Text } from 'react-native-elements';
 import firebase from "firebase/compat/app"
@@ -8,9 +8,9 @@ import "firebase/compat/firestore"
 import { getAuth, signOut } from "firebase/auth";
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { FontAwesome } from '@expo/vector-icons';
-import SelectMedicamento from '../../componentes/slots/selectmedicamento';
 import { child, getDatabase, ref, set } from 'firebase/database';
-import teste from '../../componentes/slots/selectmedicamento';
+import DropDownPicker from 'react-native-dropdown-picker';
+import DatePicker from 'react-native-datepicker';
 
 
 
@@ -21,9 +21,27 @@ export interface updateSlotProps {
 export function UpdateSlot (props: updateSlotProps) {
     
   const nav = useNavigation();
+  const [date, setDate] = useState('09-10-2020');
   const auth = getAuth()
   const database = getDatabase();
   const usuarioID = auth.currentUser.uid;
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [dadosMedicamento,setdadosMedicamento] = useState([])
+  const [items, setItems] = useState([
+    {label: 'Dipirona', value: 'Dipirona'},
+    {label: 'Buprofinule', value: 'Buprofinule'},
+    {label: 'medicamento1', value: 'medicamento1'},
+    {label: 'medicamento2', value: 'medicamento2'},
+    {label: 'medicamento3', value: 'medicamento3'},
+    {label: 'medicamento4', value: 'medicamento4'},
+    {label: 'medicamento5', value: 'medicamento5'},
+    {label: 'medicamento6', value: 'medicamento6'},
+    {label: 'medicamento7', value: 'medicamento7'},
+    {label: 'medicamento8', value: 'medicamento8'},
+    {label: 'medicamento9', value: 'medicamento9'},
+    {label: 'medicamento10', value: 'medicamento10'},
+  ]);
 
   
 
@@ -31,15 +49,17 @@ export function UpdateSlot (props: updateSlotProps) {
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     //online
-    console.log( user );
+    //console.log( user );
   } else {
     //offline
   }
   });
 
   
-  const Save = async () => {   
+  const Save = async () => {  
     
+    console.log(date)
+    console.log(dadosMedicamento)
 
   }
 
@@ -59,14 +79,86 @@ firebase.auth().onAuthStateChanged(function(user) {
         </View>
         <View style={styles.divisor1}>
         <Text style={styles.title2}>Selecione a Hora</Text>
-        <Text style={styles.title2}>Selecione a Data</Text>
+        <DatePicker
+          style={styles.datePickerStyle}
+          date={date} //initial date from state
+          mode="date" //The enum of date, datetime and time
+          placeholder="Selecione a data"
+          format="DD-MM-YYYY"
+          confirmBtnText="Confirmar"
+          cancelBtnText="Cancelar"
+          customStyles={{
+            dateIcon: {
+              //display: 'none',
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0,
+            },
+            dateInput: {
+              marginLeft: 36,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate(date);
+          }}
+        />
         </View>       	
       </View>
       <Text style={{textAlign: 'center',fontSize: 14,fontWeight: 'bold',color: '#DEDBDB'}}>Medicamentos</Text>
       <View style={styles.container}>
-        <SelectMedicamento/>
-             
+      <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      paddingHorizontal: 5,
+
+    }}>
+      <DropDownPicker style={{marginTop:10,minHeight:30, backgroundColor:'#0077B6',borderColor:'white', borderWidth:0.1, borderRadius:0,maxHeight:'100%'}}
+        open={open}
+        value={value}
+        items={items}
+        onClose={() =>           
+          setdadosMedicamento(value)
+          
+        }
+        onChangeValue={(value) => {
+          console.log(value);
+        }        
+        }     
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        listMode="MODAL"
+        searchable={true}
+        max={7}
+        modalContentContainerStyle={{
+          backgroundColor: "#0077B6",
+          flex:0,
+        }}
+        
+        placeholder="Selecione os medicamentos"
+        placeholderStyle={{
+        color: "white",      
+        fontWeight: "bold"
+        }}  
+        extendableBadgeContainer={true}
+        multiple={true}
+        mode="BADGE"
+        badgeStyle={{
+          backgroundColor:'white',
+          borderColor:'white',
+          borderWidth: 0.8
+        }}
+        badgeColors="#00B4D8"
+        badgeTextStyle={{
+          color:'white'
+        }}
+        badgeDotColors='#0077B6'
+      />
+      </View>             
       </View>
+      <Button title="Salvar" onPress={Save} buttonStyle={styles.btn}></Button>
 
 
     </View>
@@ -81,10 +173,8 @@ firebase.auth().onAuthStateChanged(function(user) {
       flex: 0,
       backgroundColor: '#0077B6',
       alignItems: 'center',
-      width: '80%',
-      marginLeft:'10%',
-      height: '70%',
-      marginTop: '30%',
+      width: '100%',
+      height: '100%',
       borderRadius: 15
       
     },
@@ -111,10 +201,14 @@ firebase.auth().onAuthStateChanged(function(user) {
       flexDirection: 'row',
     },
 
+    datePickerStyle: {
+      width: 200,
+    },
+
     head1:{
       alignItems:'flex-start',
       width: '70%',
-      marginTop:10,
+      marginTop:'20%',
       flexDirection: 'row',
       justifyContent: 'space-between'
     },
